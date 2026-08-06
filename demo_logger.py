0c574114c118ec79fe69732d19229006cf5b7933
ballertt <3052652983@qq.com>
2026-08-06 22:38:34 +0800

    clean up temp files

--- demo_logger.py
+++
@@ -1,4 +0,0 @@
-from common.logger import logger
-
-logger.info("框架启动")
-logger.error("这是一条错误日志")
--- test_smoke.py
+++
@@ -1,5 +0,0 @@
-# test_smoke.py
-def test_hello():
-    """最简单的测试：1+1 应该等于 2"""
-    assert 1 + 1 == 2
-    
